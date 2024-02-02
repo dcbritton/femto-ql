ecb345cfe481cbfaa4efcb9bfab46b05fa2eb461
dcbritton <122560490+dcbritton@users.noreply.github.com>
2024-02-02 13:50:07 -0500

    Add main.cpp and token.hpp

---
+++ token.hpp
@@ -0,0 +1,31 @@
+// token.hpp
+
+#include <string>
+#include <vector>
+
+enum token_type {          
+    kw_select,           
+    kw_from,             
+    kw_where,           
+    identifier,         // column name, table name, alias       
+    operator_equals,
+    int_literal,        
+    semicolon,          
+    open_parenthesis,
+    close_parenthesis
+};
+
+struct token {
+    token_type type;
+    std::string value = "";
+    token(token_type type, std::string value) : type(type), value(value) {}; // member initialization list :)
+};
+
+void print_token_stream(std::vector<token> token_stream) {
+    for (auto x : token_stream) 
+        std::cout << x.value << ' ';
+    std::cout << '\n';
+    for (auto x : token_stream) 
+        std::cout << x.type << ' ';
+    std::cout << '\n';
+}
