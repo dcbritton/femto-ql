0abaac8c935fdd3cf002c7331ea9c0f74d84dd73
dcbritton <122560490+dcbritton@users.noreply.github.com>
2024-02-04 07:55:22 +0000

    Add first draft of parse functions and grammar.txt

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "token.hpp"
 #include "tokenize.hpp"
+#include "parse.hpp"
 
 int main() {
     std::cout << "Enter your query: ";
@@ -10,6 +11,14 @@ int main() {
     std::vector<token> token_stream = tokenize(statement);
     print_token_stream(token_stream);
 
+    node* ast = parse(token_stream);
+
+    pre_order_traversal(ast);
+    std::cout << '\n';
+    post_order_traversal(ast);
+    std::cout << '\n';
+
     std::cout << "Program has ended properly.\n";
+
     return 0;
 }
---
+++ parse.hpp
@@ -0,0 +1,154 @@
+// parse.hpp
+
+#ifndef PARSE
+#define PARSE
+
+#include <vector>
+#include "token.hpp"
+
+enum node_type {
+    statement,
+    select_clause,
+    from_clause,
+    where_clause,
+    bool_expr,           
+    node_identifier, // column name, table name, alias       
+    node_op_equals,
+    node_int_literal
+};
+
+// this feels like a use for polymorphism -_-
+struct node {
+    node_type type;
+    std::string value; // empty on non-terminal symbols
+    std::vector<node*> components; // empty on terminal symbols
+    node(node_type type) : type(type) {};
+};
+
+void post_order_traversal(node* root) {
+    if (root == nullptr) return;
+
+    for (node* child : root->components) {
+       post_order_traversal(child); 
+    }
+
+    std::cout << root->type << " "; 
+}
+
+void pre_order_traversal(node* root) {
+    if (root == nullptr) return;
+
+    std::cout << root->type << " "; 
+
+    for (node* child : root->components) {
+        pre_order_traversal(child); 
+    }
+}
+
+// bool_expr -> ( bool_expr ) | identifier operator_equals int_literal
+node* parse_bool_expr(std::vector<token>::const_iterator& it) {
+    if (it->type == open_parenthesis) {
+        it++; // consume (
+        node* sub_be = parse_bool_expr(it);
+
+        if (it->type != close_parenthesis) {
+            std::cout << "Unpaired parentheses in boolean expression.\n";
+            exit(1);
+        }
+
+        it++; // consume )   
+        node* be = new node(bool_expr);
+        be->components.push_back(sub_be);
+        return be;
+    }
+
+    // TO DO: flatten
+    if (it->type == identifier) {
+        it++;
+
+        if (it->type == operator_equals) {
+            it++;
+
+            if (it->type == int_literal) {
+                it++;
+                node* id = new node(node_identifier);
+                node* oe = new node(node_op_equals);
+                node* il = new node(node_int_literal);
+                node* be = new node(bool_expr);
+                be->components.push_back(id);
+                be->components.push_back(oe);
+                be->components.push_back(il);
+                return be;
+            }
+
+            else {
+                std::cout << "Error while parsing boolean expression.\nExpected an int literal after '='.\n";
+                exit(1);
+            }
+        }
+
+        else {
+            std::cout << "Error while parsing boolean expression.\nExpected an '=' after identifier.\n";
+            exit(1);
+        }
+    }
+
+    else {
+        std::cout << "Error while parsing boolean expression.\nExpected an ( or identifier.\n";
+        exit(1);
+    }
+}
+
+// where_clause -> kw_where bool_expr
+node* parse_where_clause(std::vector<token>::const_iterator& it) {
+    // pass kw_where
+    it++;
+
+    if (it->type == identifier || it->type == open_parenthesis) {
+        node* wc = new node(where_clause);
+        wc->components.push_back(parse_bool_expr(it));
+        return wc;
+    }
+    std::cout << "Error while parsing where clause.\nExpected identifier or ( after \"where\".\n";
+    exit(1);
+}
+
+// select_clause -> kw_select identifier
+node* parse_select_clause(std::vector<token>::const_iterator& it) {
+    it++; // consume kw_select
+    if (it->type == identifier) {
+        it++; // consume identifier
+        node* id = new node(node_identifier);
+        node* sc = new node(select_clause);
+        sc->components.push_back(id);
+        return sc;
+    }
+    std::cout << "Expected identifier after \"select\".\n";
+    exit(1);
+}
+
+// from_clause -> kw_from identifier
+node* parse_from_clause(std::vector<token>::const_iterator& it) {
+    it++; // consume kw_from
+    if (it->type == identifier) {
+        it++; // consume identifier
+        node* id = new node(node_identifier);
+        node* fc = new node(from_clause);
+        fc->components.push_back(id);
+        return fc;
+    }
+    std::cout << "Expected identifier after \"from\".\n";
+    exit(1);
+}
+
+// statement -> select_clause from_clause where_clause
+node* parse(std::vector<token> tokens) {
+    std::vector<token>::const_iterator it = tokens.begin();
+    node* ast = new node(statement);
+    ast->components.push_back(parse_select_clause(it));
+    ast->components.push_back(parse_from_clause(it));
+    ast->components.push_back(parse_where_clause(it));
+    return ast;
+}
+
+#endif
