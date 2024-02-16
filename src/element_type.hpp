c933b5e00c422579621b1762085560114969faa0
dcbritton <122560490+dcbritton@users.noreply.github.com>
2024-02-16 23:37:09 +0000

    Add tokenTypeToString() for consume() errors

--- src/element_type.hpp
+++ src/element_type.hpp
@@ -48,9 +48,52 @@ enum element_type {
     op_less_than_equals = 53,
     op_greater_than = 54,
     op_greater_than_equals = 55,
+
     op_and = 56,
     op_or = 57,
     op_not = 58
 };
+
+std::string tokenTypeToString(element_type type) {
+    switch (type) {
+        case kw_select: return "kw_select";
+        case kw_from: return "kw_from";
+        case kw_where: return "kw_where";
+        case kw_distinct: return "kw_distinct";
+        case kw_order: return "kw_order";
+        case kw_asc: return "kw_asc";
+        case kw_desc: return "kw_desc";
+        case kw_in: return "kw_in";
+        case kw_any: return "kw_any";
+        case kw_all: return "kw_all";
+
+        case identifier: return "identifier";
+        case int_literal: return "int_literal";
+        case chars_literal: return "chars_literal";
+        case float_literal: return "float_literal";
+        case kw_true: return "kw_true";
+        case kw_false: return "kw_false";
+        case kw_null: return "kw_null";
+
+        case semicolon: return "semicolon";
+        case open_parenthesis: return "open_parenthesis";
+        case close_parenthesis: return "close_parenthesis";
+        case asterisk: return "asterisk";
+        case comma: return "comma";
+        
+        case op_equals: return "op_equals";
+        case op_not_equals: return "op_not_equals";
+        case op_less_than: return "op_less_than";
+        case op_less_than_equals: return "op_less_than_equals";
+        case op_greater_than: return "op_greater_than";
+        case op_greater_than_equals: return "op_greater_than_equals";
+
+        case op_and: return "op_and";
+        case op_or: return "op_or";
+        case op_not: return "op_not";
+
+        default: return "Unknown type";
+    }
+}
 
 #endif
