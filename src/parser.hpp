9175bf0c6dfe2179f9b35ec9a2ba4ee41ef4f9bc
dcbritton <122560490+dcbritton@users.noreply.github.com>
2024-02-17 07:05:18 +0000

    Fix error output when comment is on final line of selection

--- src/parser.hpp
+++ src/parser.hpp
@@ -207,6 +207,12 @@ public:
     }
 
     void discard(element_type expected_type) {
+
+        if (it == tokens.end()) {
+            std::cout << "Parser error. Unexpected end of input after " << tokenTypeToString((it-1)->type) << ".\n";
+            exit(1);
+        }
+
         if (it->type != expected_type) {
             std::cout << "Parser error on line " << it->line_number 
                     << ". Expected a(n) " << tokenTypeToString(expected_type) 
@@ -217,6 +223,12 @@ public:
     }
 
     void consume(element_type expected_type, std::vector<std::shared_ptr<node>>& components) {
+
+        if (it == tokens.end()) {
+            std::cout << "Parser error. Unexpected end of input after " << tokenTypeToString((it-1)->type) << ".\n";
+            exit(1);
+        }
+
         if (it->type != expected_type) {
             std::cout << "Parser error on line " << it->line_number 
                     << ". Expected a(n) " << tokenTypeToString(expected_type) 
@@ -228,6 +240,12 @@ public:
     }
 
     void consume_optional(element_type expected_type, std::vector<std::shared_ptr<node>>& components) {
+        
+        if (it == tokens.end()) {
+            std::cout << "Parser error. Unexpected end of input after " << tokenTypeToString((it-1)->type) << ".\n";
+            exit(1);
+        }
+
         if (it->type == expected_type) {
             components.push_back(std::make_shared<node>(it->type));
             ++it; // consume token
@@ -235,11 +253,5 @@ public:
     }
 
 };
-
-
-
-
-
-
 
 #endif
