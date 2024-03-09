6afa0b063c5f763fba60896bafc5e5b89e2546fb
dcbritton <122560490+dcbritton@users.noreply.github.com>
2024-03-08 23:27:51 -0500

    Add first set of validation checks for creations

--- src/validate.hpp
+++ src/validate.hpp
@@ -30,6 +30,10 @@ public:
                 case drop:
                     validateDrop(nodePtr);
                     break;
+
+                case creation:
+                    validateCreation(nodePtr);
+                    break;
                 
                 default:
                     std::cout << "Validation error. Tried to validate an unknown statement type: " << tokenTypeToString(nodePtr->type) << ".\n";
@@ -39,6 +43,39 @@ public:
     }
 
     // a million validation functions
+
+    // validate create (original)
+    void validateCreation(std::shared_ptr<node> creationRoot) {
+        std::string tableName = creationRoot->components[0]->value;
+        auto p = find(persistents, tableName);
+
+        // if table already exists in permanents, can't create
+        if (p != persistents.end()) {
+            std::cout << "Validation error. Table \"" << tableName << "\" already exists.\n";
+            exit(1);
+        }
+
+        // no <= 0 length chars
+        // loop through all columns of the table, if chars, check that -> type >= 1
+        for (auto columnTypePair : creationRoot->components[1]->components) {
+            if (columnTypePair->components[1]->type == kw_chars) {
+                if (stoi(columnTypePair->components[2]->value) <= 0) {
+                    std::cout << "Validation error. Column \"" << columnTypePair->components[0]->value << "\" in created table \"" << creationRoot->components[0]->value << "\" may not have a non-positive number of columns.\n";
+                    exit(1);
+                }
+            }
+        }
+
+        // @TODO
+        // Cannot create a table from a definition that does not exist.
+
+        // add table to permanents
+        // @TODO overload []
+        // @TODO nodeToTable(table)
+        // persistents.push_back(nodeToTable(???));
+
+        std::cout << "Creation validated.\n"; 
+    }
 
     // validate drop statement
     void validateDrop(std::shared_ptr<node> dropRoot) {
