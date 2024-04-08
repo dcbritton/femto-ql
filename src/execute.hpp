b5ae05787b09e5f4101c68387d2f42ff31af8c5a
dcbritton <122560490+dcbritton@users.noreply.github.com>
2024-04-07 22:57:08 -0400

    Add execution of inserts for ints and floats (all with values, no nulls)

--- src/execute.hpp
+++ src/execute.hpp
@@ -14,17 +14,74 @@
 std::string DIRECTORY = "../tables/";
 std::string FILE_EXTENSION = ".ftbl";
 
+void writeValue(const std::string& value, element_type type, std::ofstream& file) {
+    switch (type) {
+        case int_literal: {
+            int inum = stoi(value);
+            file.write(reinterpret_cast<const char*>(&inum), sizeof(inum));
+            break;
+        }
+        case float_literal: {
+            float fnum = stof(value);
+            file.write(reinterpret_cast<const char*>(&fnum), sizeof(fnum));
+            break;
+        }
+        case chars_literal: {
+            break;
+        }
+        case bool_literal: {
+            break;
+        }
+    }
+}
+
 void insert(std::shared_ptr<node> insertRoot) {
+
     std::string tableName = insertRoot->components[0]->value;
+    std::ofstream file(DIRECTORY + tableName + FILE_EXTENSION, std::ios_base::app);
+
+    // write the 'delete' byte as 0
+    file << static_cast<unsigned char>(0b0);
+
     auto columnValueListRoot = insertRoot->components[1];
 
-    // auto t = find();
+    auto tables = buildTableList(DIRECTORY);
+    auto t = find(tableName, tables);
+    for (auto& c : t->columns) {
+        for (auto& columnValuePair : columnValueListRoot->components) {
+            // @TODO simplify logic
+            int numBytesToWrite = 4;
+            // column mentioned
+            if (columnValuePair->components[0]->value == c.name) {
+                // null insert, write null byte as 1, fill rest as 0
+                if (columnValuePair->components[1]->type == kw_null) {
+                    //     file << static_cast<unsigned char>(0b1);
+                    // if (c.type == chars_literal)
+                    //     numBytesToWrite = c.charsLength;
+                    // for (int i = 0; i < numBytesToWrite; ++i)
+                    //     file << static_cast<unsigned char>(0b0);
+                }
+                // value to insert
+                else {
+                    file << static_cast<unsigned char>(0b0);
+                    writeValue(columnValuePair->components[1]->value, c.type, file);
+                }
+            }
+            // otherwise, write null byte as 1, fill rest as 0
+            else {
+                // file << static_cast<unsigned char>(0b1);
+                // if (c.type == chars_literal)
+                //     numBytesToWrite = c.charsLength;
+                // for (int i = 0; i < numBytesToWrite; ++i)
+                //     file << static_cast<unsigned char>(0b0);
+            }
+        }
+    }
 }
 
 void executeDrop(std::shared_ptr<node> dropRoot)  {
-    std::filesystem::remove("/tables/" + dropRoot->components[0]->value + ".ftbl");
+    std::filesystem::remove("../tables/" + dropRoot->components[0]->value + ".ftbl");
 }
-
 
 void define(std::shared_ptr<node> definitionRoot) {
     
