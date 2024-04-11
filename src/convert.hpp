0880864f03ab6a8758ca446dbae87b39202fd5b1
dcbritton <122560490+dcbritton@users.noreply.github.com>
2024-04-11 13:40:17 -0400

    Rename EntryIterator& parameter in convert()

--- src/convert.hpp
+++ src/convert.hpp
@@ -8,35 +8,35 @@
 #include "entry_iterator.hpp"
 #include "EvaluationNode.hpp"
 
-std::shared_ptr<EvaluationNode> convert(std::shared_ptr<node> boolExprRoot, EntryIterator& entry) {
+std::shared_ptr<EvaluationNode> convert(std::shared_ptr<node> boolExprRoot, EntryIterator& entryIteratorReference) {
 
     // (bool_expr)
     if (boolExprRoot->components.size() == 1 /*only child: boolExprRoot->components[0]->type == bool_expr*/) {
         ParensNode pn;
-        pn.subExpr = convert(boolExprRoot->components[0], entry);
+        pn.subExpr = convert(boolExprRoot->components[0], entryIteratorReference);
         return std::make_shared<ParensNode>(pn);
     }
 
     // !(bool_expr)
     else if (boolExprRoot->components[0]->type == op_not) {
         NotNode nn;
-        nn.subExpr = convert(boolExprRoot->components[1], entry);
+        nn.subExpr = convert(boolExprRoot->components[1], entryIteratorReference);
         return std::make_shared<NotNode>(nn);
     }
 
     // bool_expr op_or bool_expr
     else if (boolExprRoot->components[1]->type == op_or) {
         OrNode on;
-        on.lhs = convert(boolExprRoot->components[0], entry);
-        on.rhs = convert(boolExprRoot->components[2], entry);
+        on.lhs = convert(boolExprRoot->components[0], entryIteratorReference);
+        on.rhs = convert(boolExprRoot->components[2], entryIteratorReference);
         return std::make_shared<OrNode>(on);
     }
 
     // bool_expr op_and bool_expr
     else if (boolExprRoot->components[1]->type == op_and) {
         AndNode an;
-        an.lhs = convert(boolExprRoot->components[0], entry);
-        an.rhs = convert(boolExprRoot->components[2], entry);
+        an.lhs = convert(boolExprRoot->components[0], entryIteratorReference);
+        an.rhs = convert(boolExprRoot->components[2], entryIteratorReference);
         return std::make_shared<AndNode>(an);
     }
 
@@ -54,33 +54,34 @@ std::shared_ptr<EvaluationNode> convert(std::shared_ptr<node> boolExprRoot, Entr
     else {
         // rhs identifier
         if (boolExprRoot->components[2]->type == identifier) {
+            // @TODO NEED TO KNOW TYPE, PASS TABLE INTO CONVERT!
 
         }
 
         // rhs int literal
         else if (boolExprRoot->components[2]->type == int_literal) {
-            return std::make_shared<IntLiteralComparisonNode>(boolExprRoot->components[0]->value, boolExprRoot->components[1]->type, stoi(boolExprRoot->components[2]->value), entry);
+            return std::make_shared<IntLiteralComparisonNode>(boolExprRoot->components[0]->value, boolExprRoot->components[1]->type, stoi(boolExprRoot->components[2]->value), entryIteratorReference);
         }
 
         // rhs float literal
         else if (boolExprRoot->components[2]->type == float_literal) {
-            return std::make_shared<FloatLiteralComparisonNode>(boolExprRoot->components[0]->value, boolExprRoot->components[1]->type, stof(boolExprRoot->components[2]->value), entry);
+            return std::make_shared<FloatLiteralComparisonNode>(boolExprRoot->components[0]->value, boolExprRoot->components[1]->type, stof(boolExprRoot->components[2]->value), entryIteratorReference);
         }
 
         // rhs chars literal
         else if (boolExprRoot->components[2]->type == chars_literal) {
-            return std::make_shared<CharsLiteralComparisonNode>(boolExprRoot->components[0]->value, boolExprRoot->components[1]->type, boolExprRoot->components[2]->value, entry);
+            return std::make_shared<CharsLiteralComparisonNode>(boolExprRoot->components[0]->value, boolExprRoot->components[1]->type, boolExprRoot->components[2]->value, entryIteratorReference);
         }
 
         // rhs bool literal
         else if (boolExprRoot->components[2]->type == bool_literal) {
             bool value = boolExprRoot->components[2]->value == "true";
-            return std::make_shared<BoolLiteralComparisonNode>(boolExprRoot->components[0]->value, boolExprRoot->components[1]->type, value, entry);
+            return std::make_shared<BoolLiteralComparisonNode>(boolExprRoot->components[0]->value, boolExprRoot->components[1]->type, value, entryIteratorReference);
         }
 
         // null comparison
         else if (boolExprRoot->components[2]->type == kw_null) {
-            return std::make_shared<TypeAgnosticNullComparisonNode>(boolExprRoot->components[0]->value, boolExprRoot->components[1]->type, entry);
+            return std::make_shared<TypeAgnosticNullComparisonNode>(boolExprRoot->components[0]->value, boolExprRoot->components[1]->type, entryIteratorReference);
         }
     }
 }
